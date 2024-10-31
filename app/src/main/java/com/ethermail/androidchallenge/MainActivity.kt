c3b71619923db056201173ef4ce00f3cdee8aae9
codeItRalf <raffe_83@hotmail.com>
2024-10-31 08:17:43 +0100

    Initial commit

---
+++ app/src/main/java/com/ethermail/androidchallenge/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.ethermail.androidchallenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.ethermail.androidchallenge.ui.theme.features.assets.AssetsScreen
+import com.ethermail.androidchallenge.ui.theme.features.assets.dummyAssets
+import com.ethermail.androidchallenge.ui.theme.AndroidChallengeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidChallengeTheme {
+                // TODO: To be replaced with the real data from CoinCap API as part of the code challenge
+                // TODO: implement navigation strategy for the application
+                AssetsScreen(assets = dummyAssets)
+            }
+        }
+    }
+}
